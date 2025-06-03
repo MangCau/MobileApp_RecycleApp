bbbeec194cdc1456629e21818301cd20e03369b6
minhle2212044 <lecongminh852004@gmail.com>
2025-06-03 22:02:58 +0700

    Expo update ci

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
 import { Redirect } from "expo-router";
-import { SafeAreaView, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, ActivityIndicator } from "react-native";
 
 export default function Index() {
-  return (
-    <Redirect href="/onboarding"/>
-  );
+  const [loading, setLoading] = useState(true);
+  const [hasSeenOnboarding, setHasSeenOnboarding] = useState(false);
+
+  useEffect(() => {
+    const checkOnboarding = async () => {
+      const seen = await AsyncStorage.getItem("hasSeenOnboarding");
+      setHasSeenOnboarding(seen === "true");
+      setLoading(false);
+    };
+    checkOnboarding();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <Redirect href={hasSeenOnboarding ? "/screen/login" : "/onboarding"} />;
 }
